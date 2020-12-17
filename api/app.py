fcc6e07264420f12bec8be8f05df721f0a8d6b2e
ecleek <eric_cleek@steris.com>
2020-12-17 16:17:19 -0500

    Flask-cors added and enabled

--- api/app.py
+++ api/app.py
@@ -1,17 +1,20 @@
 
 from flask import Flask
-from flask import request
+from flask import request, jsonify
 from random import getrandbits
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/api/submit', methods=['post'])
 def submit_info():
-    hash = getrandbits(32)
-    f = request.files['image']
-    file_path = '../data/' + str(hash) + ".dcm"
-    f.save(file_path)
-    return file_path
+    return(jsonify(request.json))
+    # hash = getrandbits(32)
+    # f = request.files['image']
+    # file_path = '../data/' + str(hash) + ".dcm"
+    # f.save(file_path)
+    # return file_path
 
 @app.route('/api', methods=['get'])
 def test():
