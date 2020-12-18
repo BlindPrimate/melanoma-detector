9658d369486d03526bb0849056ce6114923af0e4
ecleek <eric_cleek@steris.com>
2020-12-18 16:52:03 -0500

    added image paths

--- api/melanomaModel/__init__.py
+++ api/melanomaModel/__init__.py
@@ -1,5 +1,5 @@
 import numpy as np
-# import tensorflow as tf
+import tensorflow as tf
 import pandas as pd
 import matplotlib.pyplot as plt
 import pydicom as dicom
@@ -18,6 +18,22 @@ data_path = os.path.join(file_path, "data", "siim-isic-melanoma-classification")
 df = pd.read_csv(os.path.join(data_path, "train.csv"))
 
 print(df.head())
+
+images = []
+for x in df['image_name']:
+    image = os.path.join(data_path, 'train',  x + '.dcm')
+    ds = dicom.dcmread(image)
+    pixels = ds.pixel_array
+    images.append(pixels.flatten())
+
+images = tf.keras.preprocessing.sequence.pad_sequences(
+  images,
+  maxlen = 720,
+  dtype = "int32",
+  padding = "pre",
+  truncating = "pre",
+  value = 0
+)
 
 if __name__ == '__main__':
     pass
